b1e06e45ffe93e16e12a6db61887be95c9593ca8
Kyle Liu <KylesLiu@outlook.com>
2018-03-23 20:08:44 +0800

    Daily Commit

---
+++ src/sort/Bubble.java
@@ -0,0 +1,18 @@
+package sort;
+
+/**
+* 
+* Title: Bubble Sort
+* Description:
+* @author Kyle Liu  
+* @date Mar 23, 2018
+*/
+
+public class Bubble {
+
+	public static void main(String[] args) {
+		System.out.println("Bubble Sort");
+
+	}
+
+}
---
+++ src/tree/Binary.java
@@ -0,0 +1,17 @@
+package tree;
+
+/**
+* 
+* Title: Binary Tree
+* Description:
+* @author Kyle Liu  
+* @date Mar 23, 2018
+*/
+public class Binary {
+
+	public static void main(String[] args) {
+		System.out.println("Binary Tree");
+
+	}
+
+}
