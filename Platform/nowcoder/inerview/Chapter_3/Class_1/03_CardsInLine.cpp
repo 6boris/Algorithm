c11cd0807b9accf1b45086fd4c24f7b580b650e4
Kayle Liu <280183608@qq.com>
2018-05-17 13:20:06 +0800

    Fix Some Confilet

---
+++ Platform/nowcoder/inerview/Chapter_3/Class_1/03_CardsInLine.cpp
@@ -0,0 +1,42 @@
+#include<algorithm>
+#include<cmath>
+#include<deque>
+#include<iostream>
+#include<set>
+#include<string>
+#include<sstream>
+#include<stack>
+#include<string.h>
+#include<vector>
+#include<map>
+#include<utility>
+#include<queue>
+#include<iterator>
+#include<math.h>
+#include<malloc.h>
+#define TIME std::ios::sync_with_stdio(false)
+#define LL long long
+#define MAX 233
+#define INF 0x3f3f3f3f
+
+using namespace std;
+
+class Solution {
+public:
+
+private:
+
+};
+
+
+int main()
+{
+    Solution s;
+    vector<int> arr = {1,2,3,4,5,6,7,8};
+
+    for (int i=0 ; i<arr.size() ; i++) {
+        cout<<" "<<arr[i];
+    }
+
+    return 0;
+}
